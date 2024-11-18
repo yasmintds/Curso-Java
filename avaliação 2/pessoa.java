cdfd7258a97f5e5116c175b831a34b5c74a3311a
yasmintds <162698196+yasmintds@users.noreply.github.com>
2024-11-18 08:34:37 -0300

    Add files via upload

---
+++ avaliação 2/pessoa.java
@@ -0,0 +1,14 @@
+public class pessoa {
+  private String nome;
+
+  // Getter para o nome
+  public String getNome() {
+    return nome;
+  }
+
+  // Setter para o nome
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+}
